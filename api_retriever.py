ca1a4a6abdb27eccdd2c2dc153b599990167874f
pdebeaumont <philippe.debeaumont@outlook.fr>
2023-07-28 10:20:18 +0200

    Initial commit

--- api_retriever.py
+++ api_retriever.py
@@ -4,10 +4,50 @@ from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.contrib.operators.gcs_to_bq import GoogleCloudStorageToBigQueryOperator
 from api_call_to_gcs import call_api_and_upload_to_gcs
+import requests
+import pandas as pd
+from google.cloud import storage
+
+def call_api_and_upload_to_gcs():
+    # Your API call to get data
+    response = requests.get('https://opensky-network.org/api/states/all')
+    data = response.json()['states']
+    now = datetime.now()
+    formatted_date = now.strftime("%Y-%m-%d %H:%M")
+
+    schema = {
+    'icao24': str,
+    'callsign': str,
+    'origin_country': str,
+    'time_position': 'Int64',
+    'last_contact': 'Int64',
+    'longitude': float,
+    'latitude': float,
+    'geo_altitude': float,
+    'on_ground': bool,
+    'velocity': float,
+    'true_track': float,
+    'vertical_rate': float,
+    'sensors': str,
+    'baro_altitude': float,
+    'squawk': str,
+    'spi': bool,
+    'category': 'Int64',
+    }
+
+    df = pd.DataFrame(data, columns=schema.keys()).astype(schema)
+    csv_content = df.to_csv(index=False)
+
+    # Upload data to Google Cloud Storage
+    gcs_client = storage.Client()
+    bucket_name = 'opensky-api-data'
+    blob_name = f'hourly-extraction/{formatted_date}-opensky_data.csv'
+    bucket = gcs_client.get_bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+    blob.upload_from_string(csv_content)
 
 now = datetime.now()
 formatted_date = now.strftime("%Y-%m-%d %H:%M")
-
 
 default_args = {
     'owner': 'airflow',
--- setup.sh
+++ setup.sh
@@ -1,8 +1,31 @@
 sudo apt-get install pip
 sudo apt-get install git
 sudo apt-get install python3-venv
+sudo apt-get install -y supervisor
 git clone https://github.com/philippedebeaumont/airflow-gcp.git
 cd airflow-gcp
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
+airflow db init
+cd ..
+mkdir airflow/dags
+cp airflow-gcp/api_call_to_gcs.py airflow/dags/
+sudo mkdir -p /var/log/airflow
+export USERNAME=$(whoami)
+sudo cp airflow-gcp/airflow.conf /etc/supervisor/conf.d/
+sudo nano airflow/.env
+
+airflow users create \
+  --username airflow1 \
+  --firstname YourFirstName \
+  --lastname YourLastName \
+  --email your_email@example.com \
+  --role Admin \
+  --password airflow1
+
+
+sudo supervisorctl reread
+sudo supervisorctl update
+sudo supervisorctl start airflow-webserver
+sudo supervisorctl start airflow-scheduler
