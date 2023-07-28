17288b9ed937aa6bc1aaabfdbcc62d2457d265d9
pdebeaumont <philippe.debeaumont@outlook.fr>
2023-07-28 15:05:32 +0200

    Simplified setup

--- setup/setup.sh
+++ setup/setup.sh
@@ -8,8 +8,7 @@ sudo systemctl start docker
 sudo systemctl enable docker
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-
 sudo apt-get install git
 git clone https://github.com/philippedebeaumont/airflow-gcp.git
-cd airflow-gcp
+cd airflow-gcp/airflow
 sudo docker-compose up airflow-init
