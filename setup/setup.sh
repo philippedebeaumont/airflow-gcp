1909dcbdb1c9b462f7ad48ae0664f9ac3878a586
pdebeaumont <philippe.debeaumont@outlook.fr>
2023-07-31 15:25:57 +0200

    Suppressed unnecessary variables

--- setup/setup.sh
+++ setup/setup.sh
@@ -11,5 +11,4 @@ cd airflow-gcp/airflow
 echo "" | sudo tee -a .env
 echo "BUCKET_ID=$BUCKET_ID" | sudo tee -a .env
 echo "DATASET_ID=$DATASET_ID" | sudo tee -a .env
-echo "TABLE_ID=$TABLE_ID" | sudo tee -a .env
 sudo docker-compose up -d
