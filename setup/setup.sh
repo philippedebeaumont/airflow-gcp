c8b85eab5f3265f0811a680027450ab9872c17db
pdebeaumont <philippe.debeaumont@outlook.fr>
2023-07-31 17:51:28 +0200

    Added shebang

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,10 +1,5 @@
-BUCKET_ID=$(curl -H "Metadata-Flavor: Google" "http://metadata/computeMetadata/v1/instance/attributes/BUCKET_ID")
-DATASET_ID=$(curl -H "Metadata-Flavor: Google" "http://metadata/computeMetadata/v1/instance/attributes/DATASET_ID")
-TABLE_ID=$(curl -H "Metadata-Flavor: Google" "http://metadata/computeMetadata/v1/instance/attributes/TABLE_ID")
-echo "" | sudo tee -a .env
-echo "BUCKET_ID=$BUCKET_ID" | sudo tee -a .env
-echo "DATASET_ID=$DATASET_ID" | sudo tee -a .env
-echo "TABLE_ID=$TABLE_ID" | sudo tee -a .env
+#!/bin/bash
+
 sudo apt update
 sudo apt-get -y install docker.io
 sudo groupadd docker
@@ -15,4 +10,11 @@ sudo chmod +x /usr/local/bin/docker-compose
 sudo apt-get install git
 git clone https://github.com/philippedebeaumont/airflow-gcp.git
 cd airflow-gcp/airflow
+BUCKET_ID=$(curl -H "Metadata-Flavor: Google" "http://metadata/computeMetadata/v1/instance/attributes/BUCKET_ID")
+DATASET_ID=$(curl -H "Metadata-Flavor: Google" "http://metadata/computeMetadata/v1/instance/attributes/DATASET_ID")
+TABLE_ID=$(curl -H "Metadata-Flavor: Google" "http://metadata/computeMetadata/v1/instance/attributes/TABLE_ID")
+echo "" | sudo tee -a .env
+echo "BUCKET_ID=$BUCKET_ID" | sudo tee -a .env
+echo "DATASET_ID=$DATASET_ID" | sudo tee -a .env
+echo "TABLE_ID=$TABLE_ID" | sudo tee -a .env
 sudo docker-compose up -d
