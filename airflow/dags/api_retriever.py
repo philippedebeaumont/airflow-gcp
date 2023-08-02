1ffbfdb612a04fed31f3f9f05a09092a830654e9
pdebeaumont <philippe.debeaumont@outlook.fr>
2023-08-02 14:07:52 +0200

    File path typo correction

--- airflow/dags/api_retriever.py
+++ airflow/dags/api_retriever.py
@@ -54,7 +54,7 @@ def load_csv_to_bigquery(formatted_date, bucket, dataset, table):
 
     # Get the GCS bucket and blob
     bucket = storage_client.bucket(bucket)
-    blob = bucket.blob("f'hourly-extraction/{formatted_date}-opensky_data.csv'")
+    blob = bucket.blob(f'hourly-extraction/{formatted_date}-opensky_data.csv')
 
     # Define the BigQuery dataset and table
     dataset_ref = bigquery_client.dataset(dataset)
